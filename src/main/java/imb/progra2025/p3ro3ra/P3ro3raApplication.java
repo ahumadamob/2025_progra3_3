027e822d7522fcf24b1bcf5257accce377582f91
Mario Ahumada <ahumadamob@gmail.com>
2025-04-03 19:57:44 -0300

    Primer commit

---
+++ src/main/java/imb/progra2025/p3ro3ra/P3ro3raApplication.java
@@ -0,0 +1,13 @@
+package imb.progra2025.p3ro3ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ro3raApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3ro3raApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imb/progra2025/p3ro3ra/P3ro3raApplicationTests.java
@@ -0,0 +1,13 @@
+package imb.progra2025.p3ro3ra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ro3raApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
